50ad4ebc6b4af319846312864b54a3aff33001c3
BasuKallapur <basukallapur0@gmail.com>
2024-09-17 15:59:39 +0530

    added files

--- video7.java
+++ video7.java
@@ -1,3 +1,15 @@
 public class video7 {
-    
+    public static void factorial(int n){
+        int fact=1;
+        if(n>0){
+            for(int i=n; i>=1; i--){
+                fact*=i;
+            }
+            System.out.print("factorial value of " + n + " is " + fact);
+        }
+    }
+    public static void main(String[] args) {
+        int n= 4;
+        factorial(n);
+    }
 }
