aee468baff6697ddf244e0f1568e0cc0ca5ddb4b
BasuKallapur <basukallapur0@gmail.com>
2024-10-02 15:06:14 +0530

    added files

---
+++ video10.java
@@ -0,0 +1,12 @@
+public class video10 {
+    public static void main(String[] args) {
+        int[] a= new int[4];
+        a[0]= 1;
+        a[1]= 2;
+        a[2]= 4;
+        a[3]= 5;
+        for(int i=0; i<4; i++){
+            System.out.println("value at index " + i + " is " + a[i]);
+        }
+    }
+}
